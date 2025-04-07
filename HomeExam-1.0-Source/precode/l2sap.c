8b0917f74ff36e79bdb8ad366fb96fc4c2045089
Solrun Nyborg <solrunny@guanin.uio.no>
2025-04-07 10:28:33 +0200

    opprettet l2sap_create

--- HomeExam-1.0-Source/precode/l2sap.c
+++ HomeExam-1.0-Source/precode/l2sap.c
@@ -12,10 +12,35 @@
  */
 static uint8_t compute_checksum( const uint8_t* frame, int len );
 
+
+
 L2SAP* l2sap_create( const char* server_ip, int server_port )
 {
-    fprintf( stderr, "%s has not been implemented yet\n", __FUNCTION__ );
-    return NULL;
+    // socket() returnerer en file descriptor
+    int socketFD = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socketFD < 0) {
+        perror("Couldn't create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialiserer pointer til struct L2SAP
+    L2SAP* l2sap = malloc(sizeof(struct L2SAP));
+    if (l2sap == NULL) {
+        perror("Error mallocing L2SAP");
+        free(l2sap);
+        return NULL;
+    }
+
+    // Initialiserer struct sockaddr_in som skal inn i l2sap
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(server_port);
+    addr.sin_addr.s_addr = inet_pton(AF_INET, server_ip, &addr);
+
+    // Tilordner variablene til socket (FD + adressen)
+    l2sap->socket = socketFD;
+    l2sap->peer_addr = addr;
+    return l2sap;
 }
 
 void l2sap_destroy(L2SAP* client)
@@ -32,8 +57,24 @@ void l2sap_destroy(L2SAP* client)
  * When the payload length and the L2Header together exceed
  * the maximum frame size L2Framesize, l2_sendto fails.
  */
+
+// L2SAP: struct for sockets
+    // int: socket, struct sockaddr_in 
+
+// sockaddr_in - tar inn AF_INET, portnummer, IPv4-adresse
+
+// sendto skal bruke en socket for å 
+
 int l2sap_sendto( L2SAP* client, const uint8_t* data, int len )
 {
+
+    struct sockaddr_in reciever = client->peer_addr;
+
+    inet_pton(AF_INET, reciever.sin_addr, &reciever);
+
+
+
+
     fprintf( stderr, "%s has not been implemented yet\n", __FUNCTION__ );
     return -1;
 }
